4c3364b0471f41fbbb9e72f95e95ffb5513e2713
mozinomer <mozinomer12@gmail.com>
2020-09-18 18:38:19 +0500

    asd

---
+++ about-template.php
@@ -0,0 +1,113 @@
+<?php /* Template Name: About */ ?>
+<?php get_header(); ?>
+
+
+<div class="banneraboutUs">
+	<div class="container">
+		<div class="contentBanner">
+			<div class="asd">
+				<h2>A passion for all things <span>digital</span> </h2>
+				<p>
+					This is the bit where we’re meant to make bold claims about how we’re reinventing the digital rule book by harnessing emerging technologies in our unfaltering pursuit of digital excellence. But everyone says that, right? Quite simply we help companies prepare for a digital first world.
+				</p>
+			</div>
+			<div class="imagesright">
+				<div class="row">
+					<div class="col-md-6">
+						<img src="https://s3-eu-west-1.amazonaws.com/engage-site-cms/craft/uploads/About/about-header1.jpg?mtime=20190108092644">
+					</div>
+					<div class="col-md-6">
+						<img src="https://s3-eu-west-1.amazonaws.com/engage-site-cms/craft/uploads/About/about-header2.jpg?mtime=20190108092645">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<div class="maincontentaboutus">
+	<div class="container">
+		<div class="contentaboutsu">
+
+			<h2>
+				Who We Are
+			</h2>
+			<p>
+				Support Live Agent is a renowned name of the digital world that provides top-notch customer support to convert more of your visitors into loyal customers. Whether you need to outsource live chat support or email marketing support, we have got you covered. We have been providing customer support services for over a decade and our dedicated approach makes us a preferred choice for many. We have served many globally known names and helped them meet their targets by providing fast, efficient, and reliable customer support as per their requirements.
+			</p>
+		</div>
+		<div class="contentaboutsu">
+			<h2>
+				Vision
+			</h2>
+			<p>
+				To become a leading name in customer support by providing every client with personalized solutions to help meet their business goals.
+			</p>
+		</div>
+		<div class="contentaboutsu">
+			<h2>
+				What We Do
+			</h2>
+			<p>
+				We boost your business growth by providing reliable customer support that you need. No business can survive and thrive without dedicated customer support and that’s what we are here for. Our team of online customer service agents has extensive experience in handling all types of communication channels including social media, emails, live chat, and inbound and outbound calls.
+			</p>
+		</div>
+		<div class="ourvalues">
+			<h2>
+				Why Choose Us
+			</h2>
+			<p>
+				When It Comes To Outsourcing Customer Support, It Is Critically Essential To Choose The Right Service Provider. Many Companies Are Providing Similar Services. But Choosing Us Can Provide You With A Competitive Edge Because Of Several Distinctive Features Such As :
+			</p>
+			<div class="row">
+				<div class="col">
+					<img src="#">
+					<h4>
+						Be Fearless
+					</h4>
+					<p>
+						We don't fear failure. In fact, we embrace it. We believe our failures lead to progress.
+					</p>
+				</div>
+				<div class="col">
+					<img src="#">
+					<h4>
+						Be Fearless
+					</h4>
+					<p>
+						We don't fear failure. In fact, we embrace it. We believe our failures lead to progress.
+					</p>
+				</div>
+				<div class="col">
+					<img src="#">
+					<h4>
+						Be Fearless
+					</h4>
+					<p>
+						We don't fear failure. In fact, we embrace it. We believe our failures lead to progress.
+					</p>
+				</div>
+				<div class="col">
+					<img src="#">
+					<h4>
+						Be Fearless
+					</h4>
+					<p>
+						We don't fear failure. In fact, we embrace it. We believe our failures lead to progress.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+
+
+
+
+<?php get_footer(); ?>
